ff8b875d04e33cf8beec6b0b55b6a528c66e9cfd
Guozifengg <961331126@qq.com>
2017-02-10 10:14:32 +0800

    添加Fragment

--- app/src/main/java/guozifeng/bwie/com/newstitleguozifeng/MainActivity.java
+++ app/src/main/java/guozifeng/bwie/com/newstitleguozifeng/MainActivity.java
@@ -1,16 +1,81 @@
 package guozifeng.bwie.com.newstitleguozifeng;
 
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import guozifeng.bwie.com.newstitleguozifeng.fragment.Find;
+import guozifeng.bwie.com.newstitleguozifeng.fragment.Focus;
+import guozifeng.bwie.com.newstitleguozifeng.fragment.Homepage;
+import guozifeng.bwie.com.newstitleguozifeng.fragment.Me;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
+
+    private TextView[] textArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("zzz","guozifeng");
+        TextView homepage = (TextView) findViewById(R.id.homepage);
+        homepage.setOnClickListener(this);
+        TextView find = (TextView) findViewById(R.id.find);
+        find.setOnClickListener(this);
+        TextView focus = (TextView) findViewById(R.id.focus);
+        focus.setOnClickListener(this);
+        TextView me = (TextView) findViewById(R.id.me);
+        me.setOnClickListener(this);
+
+        textArray = new TextView[]{homepage,find,focus,me};
+
+        addFragment(new Homepage());
+        setColor(0);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.homepage:
+                addFragment(new Homepage());
+                setColor(0);
+                break;
+            case R.id.find:
+                addFragment(new Find());
+                setColor(1);
+                break;
+            case R.id.focus:
+                addFragment(new Focus());
+                setColor(2);
+                break;
+            case R.id.me:
+                addFragment(new Me());
+                setColor(3);
+                break;
+        }
+    }
+
+    public void addFragment(Fragment fragment){
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction transaction=manager.beginTransaction();
+        transaction.replace(R.id.frame,fragment);
+        transaction.commit();
+    }
+
+    public void setColor(int index){
+        for(int i=0;i<textArray.length;i++){
+            if(i==index){
+                textArray[i].setTextColor(Color.RED);
+            }else{
+                textArray[i].setTextColor(Color.BLACK);
+            }
+        }
     }
 }
---
+++ app/src/main/java/guozifeng/bwie/com/newstitleguozifeng/fragment/Homepage.java
@@ -0,0 +1,24 @@
+package guozifeng.bwie.com.newstitleguozifeng.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import guozifeng.bwie.com.newstitleguozifeng.R;
+
+/**
+ * 姓名：郭子锋
+ * Created by Administrator on 2017/2/10.
+ * 作用：
+ */
+public class Homepage extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view1=View.inflate(getActivity(), R.layout.homepage,null);
+        return view1;
+    }
+}
