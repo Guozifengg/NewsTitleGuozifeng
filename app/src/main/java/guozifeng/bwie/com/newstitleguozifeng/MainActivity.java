7320542ecc2d690b4a16f4b0819a3f2ea4083be5
guozifeng <961331126@qq.com>
2017-02-10 18:45:25 +0800

    添加Fragment

--- app/src/main/java/guozifeng/bwie/com/newstitleguozifeng/MainActivity.java
+++ app/src/main/java/guozifeng/bwie/com/newstitleguozifeng/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import guozifeng.bwie.com.newstitleguozifeng.fragment.Find;
@@ -19,44 +21,65 @@ import guozifeng.bwie.com.newstitleguozifeng.fragment.Me;
 public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     private TextView[] textArray;
+    private ImageView[] imgArray;
+    private int[] img;
+    private int[] img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LinearLayout homell = (LinearLayout) findViewById(R.id.homell);
+        homell.setOnClickListener(this);
+        LinearLayout findll = (LinearLayout) findViewById(R.id.findll);
+        findll.setOnClickListener(this);
+        LinearLayout focusll = (LinearLayout) findViewById(R.id.focusll);
+        focusll.setOnClickListener(this);
+        LinearLayout mell = (LinearLayout) findViewById(R.id.mell);
+        mell.setOnClickListener(this);
+
         TextView homepage = (TextView) findViewById(R.id.homepage);
-        homepage.setOnClickListener(this);
         TextView find = (TextView) findViewById(R.id.find);
-        find.setOnClickListener(this);
         TextView focus = (TextView) findViewById(R.id.focus);
-        focus.setOnClickListener(this);
         TextView me = (TextView) findViewById(R.id.me);
-        me.setOnClickListener(this);
+
+        ImageView homeimg = (ImageView) findViewById(R.id.homeimg);
+        ImageView findimg = (ImageView) findViewById(R.id.findimg);
+        ImageView focusimg = (ImageView) findViewById(R.id.focusimg);
+        ImageView meimg = (ImageView) findViewById(R.id.meimg);
 
         textArray = new TextView[]{homepage,find,focus,me};
 
+        imgArray = new ImageView[] {homeimg,findimg,focusimg,meimg};
+        img = new int[]{R.mipmap.shou1,R.mipmap.vip1,R.mipmap.guanzhu1,R.mipmap.wode1};
+        img1 = new int[]{R.mipmap.shou,R.mipmap.vip,R.mipmap.guanzhu,R.mipmap.wode};
         addFragment(new Homepage());
+        setBackground(0);
         setColor(0);
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()){
-            case R.id.homepage:
+            case R.id.homell:
                 addFragment(new Homepage());
+                setBackground(0);
                 setColor(0);
                 break;
-            case R.id.find:
+            case R.id.findll:
                 addFragment(new Find());
+                setBackground(1);
                 setColor(1);
                 break;
-            case R.id.focus:
+            case R.id.focusll:
                 addFragment(new Focus());
+                setBackground(2);
                 setColor(2);
                 break;
-            case R.id.me:
+            case R.id.mell:
                 addFragment(new Me());
+                setBackground(3);
                 setColor(3);
                 break;
         }
@@ -77,5 +100,15 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                 textArray[i].setTextColor(Color.BLACK);
             }
         }
+    }
+
+    public void setBackground(int index){
+            for(int i=0;i<imgArray.length;i++){
+                if(i==index){
+                    imgArray[i].setImageResource(img[i]);
+                }else{
+                    imgArray[i].setImageResource(img1[i]);
+                }
+            }
     }
 }
